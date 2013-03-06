80898bc508bbbcdf0d3c2b634d64ae4d60a0c672
Christian Requena <requena@amazon.de>
2013-03-06 14:46:48 +0100

    remove config dir

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Photopoll::Application.routes.draw do
-  resources :photos do
-    resources :ratings
-  end
-
-  match '/' => 'photos#index'
-  match '/photos/search/' => 'photos#search'
-  match '/:controller(/:action(/:id))'
-end
